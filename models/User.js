5ff63d83a960f02c60c96affa8ea111a4ccb3aa6
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-04-05 23:15:44 +0600

    Created All the models

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        );
+      },
+      message: (prop) => `Invalid email: ${prop.value}`,
+    },
+  },
+  password: {
+    type: String,
+    minlength: [6, "password is too short"],
+    required: true,
+  },
+  roles: {
+    type: [String],
+    required: true,
+    default: ["Student"],
+  },
+  accountStatus: {
+    type: String,
+    enum: ["PENDING", "ACTIVE", "REJECTED"],
+    default: "PENDING",
+    required: true,
+  },
+});
+
+const User = model("User", userSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-const express = require('express');
-const connectDB = require('./db');
+const express = require("express");
+const connectDB = require("./db");
 
 const app = express();
 
 app.use(express.json());
 
-connectDB('mongodb://localhost:27017/attendance-db').then(() => {
+app.get("/", (_, res) => {
+  res.json({ status: "OK" });
+});
+
+app.get("*", (_req, _res, next) => {
+  const error = {
+    status: 404,
+    message: "Resource not found",
+  };
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  const message = err.message ? err.message : "Server Error Occured";
+  const status = err.status ? err.status : 500;
+
+  res.status(status).json({
+    message,
+  });
+});
+
+connectDB("mongodb://localhost:27017/attendance-db")
+  .then(() => {
     console.log("Database Connected!");
     app.listen(4000, () => {
-        console.log("Server is listening on port 4000");
-    })
-})
+      console.log("Server is listening on port 4000");
+    });
+  })
+  .catch((e) => console.log(e));
